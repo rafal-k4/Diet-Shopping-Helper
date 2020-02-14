1143a5366d76fa2160dc04c4bc2327c8403715c6
Rafał Kapuściński <kapuscinski.rafal@wp.pl>
2020-02-14 10:31:07 +0100

    implemented dietharmonogram service

--- src/app/diet-harmonogram.service.ts
+++ src/app/diet-harmonogram.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DietHarmonogramService {
 
-  constructor(private client: HttpClient, private config: ) { }
+  constructor(private client: HttpClient, private config: ConfigService) { }
 
   GetDietHarmonogramData() {
     console.log('test');
-    this.client.get()
+    const result = this.client.get(`https://sheets.googleapis.com/v4/spreadsheets/${this.config.SpreadSheets.DietHarmonogram.Id}/values/`
+    + `${this.config.SpreadSheets.DietHarmonogram.SheetsNames[0]}?key=${this.config.ApiKey}`);
+
+    result.subscribe(x => console.log(x));
   }
 }
