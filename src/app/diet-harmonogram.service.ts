46a08b0123f5d7384e491216e0b9eb303f43b587
Rafał Kapuściński <kapuscinski.rafal@wp.pl>
2020-02-14 10:44:58 +0100

    deleted unneccessary console log

--- src/app/diet-harmonogram.service.ts
+++ src/app/diet-harmonogram.service.ts
@@ -10,10 +10,9 @@ export class DietHarmonogramService {
   constructor(private client: HttpClient, private config: ConfigService) { }
 
   GetDietHarmonogramData() {
-    console.log('test');
     const result = this.client.get(`https://sheets.googleapis.com/v4/spreadsheets/${this.config.SpreadSheets.DietHarmonogram.Id}/values/`
     + `${this.config.SpreadSheets.DietHarmonogram.SheetsNames[0]}?key=${this.config.ApiKey}`);
-    
+
     result.subscribe(x => console.log(x));
   }
 }
