488c7b865e02a27d7405326ac214a629a325554e
Rafał Kapuściński <kapuscinski.rafal@wp.pl>
2020-02-17 16:04:52 +0100

    creating decorator provider

--- src/app/Infrastructure/Decorators.ts
+++ src/app/Infrastructure/Decorators.ts
@@ -1,7 +1,26 @@
 
 
 export function ColumnName(name: string) {
-  return (target: any, propertyKey: string | symbol) => {
+  return (target: any, propertyKey: string) => {
     console.log(target, propertyKey, name);
+    console.log(target.constructor.name);
+    console.log(ColumnNameProvider.getKeyName(target,propertyKey));
   };
+}
+
+export class ColumnNameProvider {
+
+  private static propertyToColumnName: Map<string, string>  = new Map();
+
+  static RegisterColumnName(target: any, property: string, name: string) {
+    let keyName = this.getKeyName(target, property);
+    let keys: string[] = this.propertyToColumnName.get(keyName);
+    if(!keys) {
+      
+    }
+  }
+
+  static getKeyName(target: any, property: string) {
+    return `${target.constructor.name}.${property}`;
+  }
 }
