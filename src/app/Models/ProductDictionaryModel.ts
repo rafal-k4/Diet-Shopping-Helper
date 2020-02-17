2346b45be6c402a26d7aa3e76c228ff678472bc0
Rafał Kapuściński <kapuscinski.rafal@wp.pl>
2020-02-17 14:51:18 +0100

    assign parameters from constructor

--- src/app/Models/ProductDictionaryModel.ts
+++ src/app/Models/ProductDictionaryModel.ts
@@ -1,7 +1,17 @@
-export interface ProductDictionaryModel {
+import { timingSafeEqual } from 'crypto';
+
+export class ProductDictionaryModel {
   Id: number;
   ProductName: string;
   WeightPerItem: number;
   Unit: string;
   IsQuantityCountable: boolean;
+
+  constructor(id: number, productName: string, weightPerItem: number, unit: string, isQuantityCountable: boolean){
+    this.Id = id;
+    this.ProductName = productName;
+    this.WeightPerItem = weightPerItem;
+    this.Unit = unit;
+    this.IsQuantityCountable = isQuantityCountable;
+  }
 }
