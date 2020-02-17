498bf67f633db6d5777e260eb35923947fdbb1bb
Rafał Kapuściński <kapuscinski.rafal@wp.pl>
2020-02-17 14:57:09 +0100

    aligned parameters in constructor

--- src/app/Models/ProductDictionaryModel.ts
+++ src/app/Models/ProductDictionaryModel.ts
@@ -6,7 +6,11 @@ export class ProductDictionaryModel {
   Unit: string;
   IsQuantityCountable: boolean;
 
-  constructor(id: number = 0, productName: string = '', weightPerItem: number = 0, unit: string = '', isQuantityCountable: boolean = false){
+  constructor(id: number = 0,
+              productName: string = '',
+              weightPerItem: number = 0,
+              unit: string = '',
+              isQuantityCountable: boolean = false) {
     this.Id = id;
     this.ProductName = productName;
     this.WeightPerItem = weightPerItem;
