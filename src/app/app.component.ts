e246fc76ae2d1daa8264e3406b579e054e4645bc
Rafał Kapuściński <kapuscinski.rafal@wp.pl>
2020-02-26 15:16:24 +0100

    add host listner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'diet-shopping-helper';
+  screenWidth: number;
+
+  @HostListener('window:resize', ['$event.target'])
+  onResize(e) {
+    this.screenWidth = e.innerWidth;
+  }
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    console.log(window.screen.width, window.innerWidth);
+  }
 }
