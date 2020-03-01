07e55db0d5bb524ca4aa36e7505d3b16f50b2823
rafal-k4 <42941375+rafal-k4@users.noreply.github.com>
2020-03-01 17:35:28 +0100

    almost working but values are before change

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,8 +3,9 @@ import { DietHarmonogramService } from '../diet-harmonogram.service';
 import { DietHarmonogramModel } from '../Models/DietHarmonogramModel';
 import { DayOfWeek } from '../Infrastructure/DayOfWeek';
 import { NgForm } from '@angular/forms';
-import { filter, switchMap, tap } from 'rxjs/operators';
-import { Observable, from } from 'rxjs';
+import { filter, switchMap, tap, map, flatMap } from 'rxjs/operators';
+import { Observable, from, of } from 'rxjs';
+import { fileURLToPath } from 'url';
 
 @Component({
   selector: 'app-shopping-list',
@@ -13,7 +14,7 @@ import { Observable, from } from 'rxjs';
 })
 export class ShoppingListComponent implements OnInit, AfterViewInit {
 
-  public selectedDays$;
+  public selectedDays$: Observable<DietHarmonogramModel[]>;
   public days = DayOfWeek;
   public dietDays: DietHarmonogramModel[];
   @ViewChild('dietDaysForm') form: NgForm;
@@ -29,17 +30,21 @@ export class ShoppingListComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    // this.form.valueChanges.subscribe(x => {
-    //   console.log(x);
-    //   console.log(this.dietDays)
+    // console.log(this.form.valueChanges);
+    this.selectedDays$ = this.form.valueChanges.pipe(
+      map(x => this.dietDays),
+      map(x => x.filter(y => {
+        console.log(y.Day, y.isSelected)
+        return y.isSelected}))
+    )
+
+    // this.form.valueChanges.pipe(
+    //   switchMap(x => from(this.dietDays)),
+    //   filter(x => x.isSelected),
+    //   tap(x => console.log('after filter', x)),
+    // ).subscribe(x => {
+    //   this.selectedDays$ = x;
     // })
-    this.form.valueChanges.pipe(
-      switchMap(x => from(this.dietDays)),
-      filter(x => x.isSelected),
-      tap(x => console.log('after filter', x)),
-    ).subscribe(x => {
-      this.selectedDays$ = x;
-    })
   }
 
 }
