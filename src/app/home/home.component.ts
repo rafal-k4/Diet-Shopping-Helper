6a7658f760f1cffe69955c782f0dfc07664a61a0
rafal-k4 <42941375+rafal-k4@users.noreply.github.com>
2020-04-04 19:04:19 +0200

    created separate list for filtered dietdays

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,9 @@ import { DietHarmonogramModel } from '../Models/DietHarmonogramModel';
 import { DayOfWeek } from '../Infrastructure/DayOfWeek';
 import { DictionaryProductService } from '../dictionary-product.service';
 import { ProductDictionaryModel } from '../Models/ProductDictionaryModel';
+import { Observable, Subject } from 'rxjs';
+import { ProductModel } from '../Models/ProductModel';
+import { Reflection } from '../Infrastructure/Reflection';
 
 @Component({
   selector: 'app-home',
@@ -14,20 +17,22 @@ export class HomeComponent implements OnInit {
 
   days = DayOfWeek;
 
-  dietHarmonogram: DietHarmonogramModel[];
-
+  allProductsDietHarmonogram: DietHarmonogramModel[];
   productDictionary: ProductDictionaryModel[];
 
-  filteredProducts: string[];
+  filteredListOfProducts = new Subject<DietHarmonogramModel[]>();
+
+  areProductSelected: boolean;
 
   constructor(
     private dietHarmonogramService: DietHarmonogramService,
-    private dietDictionaryService: DictionaryProductService) { }
+    private dietDictionaryService: DictionaryProductService,
+    private reflection: Reflection) { }
 
   ngOnInit(): void {
     this.dietHarmonogramService.getDietHarmonogramData({ fillRelatedObjects: true }).subscribe(
       x => {
-        this.dietHarmonogram = x;
+        this.allProductsDietHarmonogram = x;
       }
     );
 
@@ -38,7 +43,35 @@ export class HomeComponent implements OnInit {
     );
   }
 
-  receiveSelectedProducts(productsNames: number[]) {
-    console.log(productsNames);
+  receiveSelectedProducts(productsIds: number[]) {
+    this.areProductSelected = productsIds.length > 0;
+
+    const filteredProducts = this.getFilteredDietDays(productsIds);
+    this.filteredListOfProducts.next(filteredProducts);
+
+    console.log(filteredProducts);
+    console.log(productsIds);
+  }
+
+  private getFilteredDietDays(productsIds: number[]): DietHarmonogramModel[] {
+    const resultDietDays: DietHarmonogramModel[] = [];
+
+    for (const dietDay of this.allProductsDietHarmonogram) {
+      const filteredDietDay = new DietHarmonogramModel();
+      filteredDietDay.Day = dietDay.Day;
+      filteredDietDay.Products = [];
+
+      for (const productId of productsIds) {
+        if (dietDay.Products.some(x => x.ProductDictionary.Id === productId)) {
+          const selectedProduct = dietDay.Products.find(x => x.ProductDictionary.Id === productId);
+
+          filteredDietDay.Products.push(this.reflection.deepClone(selectedProduct));
+        }
+      }
+
+      resultDietDays.push(filteredDietDay);
+    }
+
+    return resultDietDays;
   }
 }
