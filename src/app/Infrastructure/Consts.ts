806862c08c878f8e0183919c797cf0115d95e134
Rafał Kapuściński <kapuscinski.rafal@wp.pl>
2020-08-15 20:36:16 +0200

    change appconfig model and add sheets for available sheets table

--- src/app/Infrastructure/Consts.ts
+++ src/app/Infrastructure/Consts.ts
@@ -15,13 +15,19 @@ export const APP_CONFIG: AppSettingsModel = {
       Id: '1BTNFS1nT6swNb_tECK7RoGRqH6nPtmCJQbOaW1wLcNI',
       SheetsCount: 1,
       SheetsNames: ['ProdDict'],
-      HeadersRowsCount: 1,
+      HeadersRowsCount: 1
     },
     DietHarmonogram: {
       Id: '1jSzKfMkQ6v5tB9Rz85uLRqAcPmbKJdbgLELJ40NwR88',
       SheetsCount: 1,
       SheetsNames: ['Variant_1'],
-      HeadersRowsCount: 1,
+      HeadersRowsCount: 1
+    },
+    AvailableDiets: {
+      Id: '1kL8IZv1qN5bJobg97jxJhE9jgx84w7rZv3h3XSmDXfU',
+      SheetsCount: 1,
+      SheetsNames: ['DietsSheetTable'],
+      HeadersRowsCount: 1
     }
   }
 };
